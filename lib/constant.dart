ae3b8c201a66709704b8dbbc909107dc1b69e8cd
TC22 <thuan.chau@sjsu.edu>
2021-02-19 18:12:49 -0800

    clean up code

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,30 +1,14 @@
 import 'package:flutter/material.dart';
 
 /// Color Theme
-const kAppBarColor = Color.fromRGBO(128, 0, 0, 1.0);
-const kBackgroundColor = Color.fromRGBO(240, 94, 92, 1.0);
-const kLabelColor = Colors.white;
-const kErrorColor = Colors.white;
-
-const kTextFieldDecoration = InputDecoration(
-  hintText: 'Enter a value',
-  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-  border: OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-  ),
-  enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.white, width: 1.0),
-    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-  ),
-  focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.white, width: 2.0),
-    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-  ),
-);
+const Color kAppBarColor = Color.fromRGBO(128, 0, 0, 1.0);
+const Color kBackgroundColor = Color.fromRGBO(240, 94, 92, 1.0);
+const Color kLabelColor = Colors.white;
+const Color kErrorColor = Colors.white;
 
 /// Database fields
-const kUserDB = 'users';
-const kEmailField = 'email';
-const kFirstNameField = 'firstname';
-const kLastNameField = 'lastname';
-const kPhoneField = 'phone';
+const String kUserDB = 'users';
+const String kEmailField = 'email';
+const String kFirstNameField = 'firstname';
+const String kLastNameField = 'lastname';
+const String kPhoneField = 'phone';
