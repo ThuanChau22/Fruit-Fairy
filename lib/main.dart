615f3042265ddde3a0ce77d84983db830b26b8d9
TC22 <thuan.chau@sjsu.edu>
2021-03-07 18:34:06 -0800

    fix merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 
 import 'package:fruitfairy/constant.dart';
 import 'package:fruitfairy/models/account.dart';
+import 'package:fruitfairy/models/basket.dart';
 import 'package:fruitfairy/screens/authentication/sign_option_screen.dart';
 import 'package:fruitfairy/screens/home_screen.dart';
 import 'package:fruitfairy/services/fireauth_service.dart';
