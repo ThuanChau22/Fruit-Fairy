cc2744dcbc6856874bc7b0001c3cc433cd4d112f
TC22 <thuan.chau@sjsu.edu>
2021-03-15 08:22:15 -0700

    add donation basket functionality

--- lib/models/fruit.dart
+++ lib/models/fruit.dart
@@ -1,8 +1,11 @@
 class Fruit {
+  static const Max = 100;
+  static const Min = 50;
   final String id;
   final String name;
   final String imagePath;
   final String imageURL;
+  int _amount = Min;
 
   Fruit({
     this.id,
@@ -10,4 +13,22 @@ class Fruit {
     this.imagePath,
     this.imageURL,
   });
+
+  int get amount {
+    return _amount;
+  }
+
+  void increase(int amount) {
+    int result = _amount + amount;
+    _amount = result < Max ? result : Max;
+  }
+
+  void decrease(int amount) {
+    int result = _amount - amount;
+    _amount = result > Min ? result : Min;
+  }
+
+  void clear() {
+    _amount = Min;
+  }
 }
---
+++ lib/widgets/rounded_icon_button.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class RoundedIconButton extends StatefulWidget {
+  final double radius;
+  final Icon icon;
+  final Color buttonColor;
+  final VoidCallback onPressed;
+
+  RoundedIconButton({
+    @required this.radius,
+    @required this.icon,
+    @required this.onPressed,
+    this.buttonColor,
+  });
+
+  @override
+  _RoundedIconButtonState createState() => _RoundedIconButtonState();
+}
+
+class _RoundedIconButtonState extends State<RoundedIconButton> {
+  Timer timer;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        HapticFeedback.mediumImpact();
+        widget.onPressed();
+      },
+      onTapDown: (details) {
+        timer = Timer.periodic(Duration(milliseconds: 200), (timer) {
+          HapticFeedback.mediumImpact();
+          widget.onPressed();
+        });
+      },
+      onTapUp: (details) {
+        timer.cancel();
+      },
+      onTapCancel: () {
+        timer.cancel();
+      },
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Material(
+          shape: CircleBorder(),
+          color: widget.buttonColor,
+          elevation: 2.0,
+          child: SizedBox(
+            width: widget.radius,
+            height: widget.radius,
+            child: widget.icon,
+          ),
+        ),
+      ),
+    );
+  }
+}
