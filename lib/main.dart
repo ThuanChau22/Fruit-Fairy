78441ad6b3858fd87e1f68d8182df5db8cde7fde
YoniGreenblum <yonatan.greenblum@sjsu.edu>
2021-03-26 16:42:13 -0700

    created multiple screens related to charity.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruitfairy/models/wish_list.dart';
 import 'package:provider/provider.dart';
 //
 import 'package:fruitfairy/constant.dart';
@@ -30,6 +31,7 @@ class FruitFairy extends StatelessWidget {
         ChangeNotifierProvider<Account>(create: (_) => Account()),
         ChangeNotifierProvider<Donation>(create: (_) => Donation()),
         ChangeNotifierProvider<Produce>(create: (_) => Produce()),
+        ChangeNotifierProvider<WishList>(create: (_) => WishList()),
       ],
       child: Authentication(),
     );
