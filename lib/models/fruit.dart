d8c5d90f4936e606bb11f90242a512e24ccbb574
TC22 <thuan.chau@sjsu.edu>
2021-04-01 03:05:36 -0700

    add comment for Fruit class

--- lib/models/fruit.dart
+++ lib/models/fruit.dart
@@ -1,5 +1,15 @@
 import 'package:meta/meta.dart';
 
+/// A class that represents a produce
+/// [id]: unique String to identify a produce
+/// in both system and database (produce name)
+/// [name]: a display name of produce
+/// [imagePath]: a path to image in storage
+/// [imageURL]: public URL for produce image
+/// [Min]: minimun percentage a donor can select
+/// [Max]: maximun percentage a donor can select
+/// [AdjustAmount]: number of percentage can be adjusted each time
+/// [_currentAmount]: current percentage of a produce
 class Fruit {
   static const Min = 25;
   static const Max = 100;
@@ -17,20 +27,26 @@ class Fruit {
     @required this.imageURL,
   });
 
+  /// Return a copy of [_currentAmount]
   int get amount {
     return _currentAmount;
   }
 
+  /// Increase [_currentAmount]
+  /// Cap at [Max]
   void increase() {
     int result = _currentAmount + AdjustAmount;
     _currentAmount = result < Max ? result : Max;
   }
 
+  /// Decrease [_currentAmount]
+  /// Cap at [Min]
   void decrease() {
     int result = _currentAmount - AdjustAmount;
     _currentAmount = result > Min ? result : Min;
   }
 
+  /// Set [_currentAmount] to default value
   void clear() {
     _currentAmount = Min;
   }
