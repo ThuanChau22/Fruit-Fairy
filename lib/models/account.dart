062f334b3a08aa4419233ad97ed6f8bb0e84a237
TC22 <thuan.chau@sjsu.edu>
2021-04-01 02:49:20 -0700

    add comment for account class

--- lib/models/account.dart
+++ lib/models/account.dart
@@ -3,7 +3,12 @@ import 'package:flutter/foundation.dart';
 //
 import 'package:fruitfairy/services/firestore_service.dart';
 
+/// A class that represents a user in the system
+/// This class is used for both donor and charity user
+/// where donor will have non-empty [_firstName] and [_lastName]
+/// and charity will have non-empty [_ein] and [_charityName]
 class Account extends ChangeNotifier {
+  /// Set default values for all fields
   String _email = '';
   String _firstName = '';
   String _lastName = '';
@@ -12,49 +17,69 @@ class Account extends ChangeNotifier {
   final Map<String, String> _phone = {};
   final Map<String, String> _address = {};
 
+  /// Return a copy of [_email]
   String get email {
     return _email;
   }
 
+  /// Return a copy of [_firstName]
   String get firstName {
     return _firstName;
   }
 
+  /// Return a copy of [_lastName]
   String get lastName {
     return _lastName;
   }
 
+  /// Return a copy of [_ein]
   String get ein {
     return _ein;
   }
 
+  /// Return a copy of [_charityName]
   String get charityName {
     return _charityName;
   }
 
+  /// Return a copy of [_phone]
+  /// A Map with keys that are declared in [FireStoreService]
   UnmodifiableMapView<String, String> get phone {
     return UnmodifiableMapView(_phone);
   }
 
+  /// Return a copy of [_address]
+  /// A Map with keys that are declared in [FireStoreService]
   UnmodifiableMapView<String, String> get address {
     return UnmodifiableMapView(_address);
   }
 
+  /// Parse account information from database
+  /// A Map with keys that are declared in [FireStoreService]
   void fromDB(Map<String, dynamic> accountData) {
+    // Clean up current data
     clear();
+
+    // Email
     _email = accountData[FireStoreService.kEmail];
+
+    // First and Last name (Donor only)
     String firstName = accountData[FireStoreService.kFirstName];
     String lastName = accountData[FireStoreService.kLastName];
     if (firstName != null && lastName != null) {
       _firstName = firstName;
       _lastName = lastName;
     }
+
+    // EIN and charity name (Charity only)
     String ein = accountData[FireStoreService.kEIN];
     String charityName = accountData[FireStoreService.kCharityName];
     if (ein != null && charityName != null) {
       _ein = ein;
       _charityName = charityName;
     }
+
+    // Phone number
     Map<String, dynamic> phone = accountData[FireStoreService.kPhone];
     if (phone != null) {
       _phone[FireStoreService.kPhoneCountry] =
@@ -64,6 +89,8 @@ class Account extends ChangeNotifier {
       _phone[FireStoreService.kPhoneNumber] =
           phone[FireStoreService.kPhoneNumber];
     }
+
+    // Address
     Map<String, dynamic> address = accountData[FireStoreService.kAddress];
     if (address != null) {
       _address[FireStoreService.kAddressStreet] =
@@ -78,6 +105,7 @@ class Account extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Set all fields to default values
   void clear() {
     _email = '';
     _firstName = '';
