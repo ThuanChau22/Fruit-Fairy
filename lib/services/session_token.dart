74e8d940de8e696a40a48c5022a6f9322456f445
TC22 <thuan.chau@sjsu.edu>
2021-04-01 04:27:52 -0700

    add comment for SessionToken class

--- lib/services/session_token.dart
+++ lib/services/session_token.dart
@@ -1,11 +1,18 @@
 import 'dart:async';
 import 'package:uuid/uuid.dart';
 
+/// A class that represents a session token
+/// [_uuid]: a unique String generator used to create session token
+/// [_timer]: a timer to keep the session alive within certain time
+/// [_sessionToken]: current session token
 class SessionToken {
   final Uuid _uuid = Uuid();
   Timer _timer;
   String _sessionToken;
 
+  /// Return a copy of [_sessionToken]
+  /// starts [_timer] and generates a new [_sessionToken]
+  /// if it is not initialized or previously terminated
   String getToken() {
     if (_sessionToken == null) {
       _sessionToken = _uuid.v4();
@@ -14,6 +21,7 @@ class SessionToken {
     return _sessionToken;
   }
 
+  /// Terminate current session token and stop timer
   void clear() {
     _sessionToken = null;
     if (_timer.isActive) {
