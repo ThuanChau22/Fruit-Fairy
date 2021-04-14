ea8b9d90680c6d1a9c40a7c09717181d8e49d56e
TC22 <thuan.chau@sjsu.edu>
2021-04-13 19:08:11 -0700

    rename + add comment

--- lib/models/charity.dart
+++ lib/models/charity.dart
@@ -2,35 +2,50 @@ import 'dart:collection';
 //
 import 'package:fruitfairy/services/firestore_service.dart';
 
+/// A class that represents a suggested charity to a donation
+/// [id]: charity document id on database
+/// [_produce]: a set of produceIds from charity wishlist
+/// [_address]: charity address
+/// [_name]: charity display name
+/// [_score]: a value that is used to rank charity suggestion priorrity
 class Charity implements Comparable<Charity> {
+  /// Set default values for all fields
   final String id;
-  String _name = '';
-  final Set<String> _produceIds = {};
+  final Set<String> _produce = {};
   final Map<String, String> _address = {};
+  String _name = '';
   double _score = 0;
 
+  /// Instantiate with charity document [id]
   Charity(this.id);
 
+  /// Return a copy of [_name]
   String get name {
     return _name;
   }
 
-  UnmodifiableSetView<String> get produceIds {
-    return UnmodifiableSetView(_produceIds);
+  /// Return a copy of [_produce] as a set
+  UnmodifiableSetView<String> get produce {
+    return UnmodifiableSetView(_produce);
   }
 
+  /// Return a copy of [_address]
+  /// A Map with keys that are declared in [FireStoreService]
   UnmodifiableMapView<String, String> get address {
     return UnmodifiableMapView(_address);
   }
 
- double get score{
+  /// Return a copy of [_score]
+  double get score {
     return _score;
- }
+  }
 
+  /// Parse [fruitId] from database
+  /// [wishlistData]: A Map with keys that are declared in [FireStoreService]
   void fromWishListsDB(Map<String, dynamic> wishlistData) {
-    if(wishlistData !=null) {
+    if (wishlistData != null) {
       wishlistData[FireStoreService.kProduceIds].forEach((fruitId) {
-        _produceIds.add(fruitId);
+        _produce.add(fruitId);
       });
     }
   }
@@ -53,12 +68,13 @@ class Charity implements Comparable<Charity> {
         address[FireStoreService.kAddressZip];
   }
 
-  void setScore(double score){
+  /// Set new ranking score
+  void setScore(double score) {
     _score = score;
   }
 
   @override
   int compareTo(Charity other) {
-    return this._score.compareTo(other._score);
+    return other._score.compareTo(this._score);
   }
 }
