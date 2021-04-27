47769b27bf9d3b5a3c77fc9f5f7312afbffedbe9
TC22 <thuan.chau@sjsu.edu>
2021-04-27 16:53:49 -0700

    re-model Produce class

--- lib/models/produce.dart
+++ lib/models/produce.dart
@@ -6,16 +6,67 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fruitfairy/models/produce_item.dart';
 
 /// A class holds a list of produce that a user can select
+/// [_produceIds]: a set of produce ids
 /// [_produce]: a map of [ProduceItem]
 /// [_subscriptions]: list of stream subcriptions that
 /// performs an opperation for each subcription on changes
+/// [_startDocument]: A cursor used to traverse DB
+/// [_endDocument]: A cursor used to traverse DB
+/// [LOAD_LIMIT]: Limit amount per donation retrieval
 class Produce extends ChangeNotifier {
+  static const LOAD_LIMIT = 12;
+  final Set<String> _produceIds = {};
   final Map<String, ProduceItem> _produce = {};
   final List<StreamSubscription<QuerySnapshot>> _subscriptions = [];
+  DocumentSnapshot _startDocument;
+  DocumentSnapshot _endDocument;
+
+  /// Return a copy of [_produceIds]
+  UnmodifiableSetView<String> get set {
+    return UnmodifiableSetView(_produceIds);
+  }
 
   /// Return a copy of [_produce]
   UnmodifiableMapView<String, ProduceItem> get map {
     return UnmodifiableMapView(_produce);
+  }
+
+  /// Return a copy of [_startDocument]
+  DocumentSnapshot get startDocument {
+    return _startDocument;
+  }
+
+  /// Return a copy of [_endDocument]
+  DocumentSnapshot get endDocument {
+    return _endDocument;
+  }
+
+  /// Add [produceId] to [_produceIds]
+  void pickProduce(String produceId) {
+    _produceIds.add(produceId);
+    notifyListeners();
+  }
+
+  /// Remove [produceId] from [_produceIds]
+  void removeProduce(String produceId) {
+    _produceIds.remove(produceId);
+    notifyListeners();
+  }
+
+  /// Add [ProduceItem] to [_produce]
+  void storeProduce(ProduceItem produceItem) {
+    _produce[produceItem.id] = produceItem;
+    notifyListeners();
+  }
+
+  /// Set [doc] as starting cursor
+  void setStartDocument(DocumentSnapshot doc) {
+    _startDocument = doc;
+  }
+
+  /// Set [doc] as ending cursor
+  void setEndDocument(DocumentSnapshot doc) {
+    _endDocument = doc;
   }
 
   /// Add [subscription] to [_subscriptions] list
@@ -31,20 +82,14 @@ class Produce extends ChangeNotifier {
     _subscriptions.clear();
   }
 
-  /// Parse [produceData] as [Map] from database
-  void fromDB(Map<String, ProduceItem> produceData) {
-    _produce.clear();
-    produceData.forEach((produceId, produceItem) {
-      _produce[produceId] = produceItem;
-    });
-    notifyListeners();
-  }
-
   /// Set object to initial state
   /// Cancel all [_subscriptions]
   void clear() {
     clearStream();
+    _produceIds.clear();
     _produce.clear();
+    _startDocument = null;
+    _endDocument = null;
     notifyListeners();
   }
 }
